1de0765d81acaaa59a098e9a434f076a08610f8d
rahulroy9202@gmail.com <rahulroy9202@gmail.com>
2016-08-28 17:13:49 +0530

    poll improvements

--- public/js/custom.js
+++ public/js/custom.js
@@ -394,7 +394,8 @@ function orderRefresh() {
     },
     error: function (xhr, _status, errorThrown) {
       console.log("err: ", {status: _status, err: errorThrown, xhr: xhr});
-      orderRefresh();
+      alert('Order Refresh Failed. WIll retry in ' + (1000 * config.order_poll_interval/2) + 'sec. Please check internet connection');
+      setTimeout(orderRefresh, 1000 * config.order_poll_interval/2);
     }
   });
 }
