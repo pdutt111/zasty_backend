537c3e5a5c896e03dc90598b06c31f46a0fc2dd8
rahulroy9202@gmail.com <rahulroy9202@gmail.com>
2016-09-04 20:56:29 +0530

    every day mod mail added

--- event_recievers/mailSender.js
+++ event_recievers/mailSender.js
@@ -7,6 +7,14 @@ var ses = require('node-ses')
     , client = ses.createClient({key: config.get('amazonses.key'), secret: config.get('amazonses.secret')});
 var db = require('../db/DbSchema');
 var userTable = db.getuserdef;
+var orderTable = db.getorderdef;
+var schedule = require('node-schedule');
+
+// for the admin email everyday :
+var j = schedule.scheduleJob('0 3 * * *', function () {
+    console.log('The answer to life, the universe, and everything! - MOD MAIL');
+    sendDailyAdminEmail();
+});
 
 // Give SES the details and let it construct the message for you.
 events.emitter.on('mail', function (data) {
@@ -40,3 +48,45 @@ events.emitter.on('mail_admin', function (data) {
         });
     });
 });
+
+function sendDailyAdminEmail() {
+    var end_date = new Date();
+    var start_date = new Date();
+    start_date.setDate(start_date.getDate() - 1);
+
+    orderTable.find({
+            created_time: {
+                $gte: start_date, $lte: end_date
+            }
+        })
+        .sort({restaurant_assigned: -1})
+        .exec(function (err, rows) {
+            var res_details = {};
+            rows.forEach(function (o) {
+
+                if (!res_details[o.restaurant_assigned])
+                    res_details[o.restaurant_assigned] = {
+                        orders: []
+                    };
+
+                res_details[o.restaurant_assigned].total_price_recieved =
+                    (res_details[o.restaurant_assigned].total_price_recieved || 0) + o.total_price_recieved;
+
+                res_details[o.restaurant_assigned].total_price_to_pay =
+                    (res_details[o.restaurant_assigned].total_price_to_pay || 0) + o.total_price_to_pay;
+
+                res_details[o.restaurant_assigned].orders.push(o);
+
+            });
+
+            var text = "this is just for your information - \n" + JSON.stringify(res_details, null, '\t');
+
+            var email = {
+                subject: "Last 24Hrs Order Details",
+                message: text,
+                plaintext: text
+            };
+
+            events.emitter.emit("mail_admin", email);
+        })
+}
