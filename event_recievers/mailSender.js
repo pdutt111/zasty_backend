d14e3037e7ebbee040243baf9675ec78e9441dda
rahulroy9202@gmail.com <rahulroy9202@gmail.com>
2016-09-04 13:02:43 +0530

    email test

--- event_recievers/mailSender.js
+++ event_recievers/mailSender.js
@@ -1,20 +1,29 @@
 /**
  * Created by pariskshitdutt on 27/08/16.
  */
-var config= require('config');
+var config = require('config');
 var events = require('../events');
 var ses = require('node-ses')
-    , client = ses.createClient({ key: config.get('amazonses.key'), secret: config.get('amazonses.secret') });
+    , client = ses.createClient({key: config.get('amazonses.key'), secret: config.get('amazonses.secret')});
 
 // Give SES the details and let it construct the message for you.
-events.emitter.on('mail',function(data){
+events.emitter.on('mail', function (data) {
 
-    //client.sendEmail({
-    //    to: data.toEmail,
-    //    from: config.get('amazonses.fromEmail'),
-    //    subject: data.subject,
-    //    message: data.message,
-    //    altText: data.plainText
-    //}, function (err, data, res) {
-    //});
+    client.sendEmail({
+        to: data.toEmail,
+        from: config.get('amazonses.fromEmail'),
+        subject: data.subject,
+        message: data.message,
+        altText: data.plainText
+    }, function (err, data, res) {
+        console.log('M', err, data, res);
+    });
 });
+
+// test
+//events.emitter.emit("mail", {
+//    toEmail: 'rahulroy9202@gmail.com',
+//    subject: 'test',
+//    message: 'test123',
+//    plainText: 'test123'
+//});
