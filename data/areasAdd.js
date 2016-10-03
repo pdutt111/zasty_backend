28e0ed6751a6f3bd2669a021abf372fe056329c6
pdutt111 <pdutt111@gmail.com>
2016-10-03 21:15:25 +0530

    mail sent to respective guys

--- data/areasAdd.js
+++ data/areasAdd.js
@@ -7,22 +7,25 @@ var db = require('../db/DbSchema');
 
 var areaTable=db.getareadef;
 var restoarea={
-    sector15:["Z0101Z2IOFF"],
-    sector48:["Z0101Z6IOCA","Z0101Z6CEC"],
-    sector56:["Z0101Z7IOSY","Z0101Z7CPP"]
+    sec15:["Z0101Z2IOFF"],
+    sec48:["Z0101Z6IOCA","Z0101Z6CEC"],
+    sec56:["Z0101Z7IOSY","Z0101Z7CPP"],
+    dlfphase2:["Z0101Z3ICMA","Z0101Z3OCG"],
+    sector45:["Z0101Z5CCP","Z0101Z5IOUK"],
+    supermart1:["Z0101Z8ICFT"]
 }
 
 //end_parsed will be emitted once parsing finished
 converter.on("end_parsed", function (jsonArray) {
     // console.log(jsonArray); //here is your result jsonarray
     for(var i=0;i<jsonArray.length;i++){
-        // console.log(jsonArray[i]);
+        console.log(jsonArray[i]);
         var area=new areaTable({
-            area: jsonArray[i].sector56,
+            area: jsonArray[i].supermart1,
             locality: "gurgaon",
             city: "gurgaon",
             country: "india",
-            serviced_by: restoarea.sector56,
+            serviced_by: restoarea.supermart1,
         });
         area.save(function(err,area,info){
             console.log(area);
@@ -32,4 +35,6 @@ converter.on("end_parsed", function (jsonArray) {
 });
 
 //read from file
-require("fs").createReadStream("./data/sec56.csv").pipe(converter);
+// for(var item in restoarea){
+    require("fs").createReadStream("./data/supermart1.csv").pipe(converter);
+// }
