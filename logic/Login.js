e1739a2423636d7bb0a04230eab416ff3e7ef33e
pdutt111 <pdutt111@gmail.com>
2016-10-17 15:24:14 +0530

    error in usercreate

--- logic/Login.js
+++ logic/Login.js
@@ -108,12 +108,12 @@ var users={
                         if(err.code==11000) {
                             userTable.findOne({email:req.body.email},"email token_validity_code fb_user_id password",function(err,user){
                                 if(!err&&user) {
-                                    if(req.body.fb_user_id==user.fb_user_id){
+                                    if(req.body.fb_user_id&&req.body.fb_user_id==user.fb_user_id){
                                         def.resolve(user);
                                         user.name=req.body.name;
                                         user.save(function(err,info){
                                         });
-                                    }else if(req.body.google_user_id==user.google_user_id){
+                                    }else if(req.body.google_user_id&&req.body.google_user_id==user.google_user_id){
                                         def.resolve(user);
                                     }else if(req.body.password){
                                         bcrypt.compare(req.body.password,user.password,function(err,res){
