9a6c2acf0df70ae883a28a4479e2ee17b2ac79ff
pdutt111 <pdutt111@gmail.com>
2016-10-20 00:55:43 +0530

    vendor.zasty redirect

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/p', express.static(path.join(__dirname, 'public/merchant')));
  */
 app.use(function (req, res, next) {
     log.info(req.subdomains);
-    if (req.subdomains.indexOf('vendor')) {
+    if (req.subdomains.indexOf('vendor')>-1) {
         res.redirect(301,'/p');
     } else {
         next();
